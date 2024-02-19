8384df4cd65224f307e863d83fe3d82231d24c35
michelsdev <sinambelamichel05@gmail.com>
2024-02-19 17:51:58 +0700

    update sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -15,7 +15,7 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item menu-open">
+          <li class="nav-item">
             <a href="#" class="nav-link">
                 <i class="nav-icon fas fa-calendar-week"></i>
               <p style="font-size: 14px">
