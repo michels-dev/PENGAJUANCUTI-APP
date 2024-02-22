3760795b4e0abfeb867a369453ac48fe42553f79
michelsdev <sinambelamichel05@gmail.com>
2024-02-22 08:11:46 +0700

    update sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -17,41 +17,12 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-                <i class="nav-icon fas fa-calendar-week"></i>
-              <p>
-                Setup Persetujuan Cuti
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="{{ route('dashboard.table-onhold') }}" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Data Cuti Awaiting Approval</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="{{ route('dashboard.table-approved') }}" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Data Cuti Approved</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="{{ route('dashboard.table-rejected') }}" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Data Cuti Rejected</p>
-                </a>
-              </li>
-            </ul>
-          </li>
           @if (Auth::user()->role == "admin")
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-fax"></i>
               <p style="font-size: 14px">
-                Manage SDM
+                Menu Approval
                 <i class="fas fa-angle-left right"></i>
               </p>
             </a>
@@ -59,12 +30,49 @@
               <li class="nav-item">
                 <a href="{{ route('admin.admin-table') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Admin Tables</p>
+                  <p>Data Cuti</p>
+                </a>
+              </li>
+            </ul>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="{{ route('admin.admin-table') }}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Data Report</p>
                 </a>
               </li>
             </ul>
           </li>
           @endif
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-calendar-week"></i>
+              <p>
+                Menu Status Cuti
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="{{ route('dashboard.table-onhold') }}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Data Awaiting Approval</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ route('dashboard.table-approved') }}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Data Approved</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ route('dashboard.table-rejected') }}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Data Rejected</p>
+                </a>
+              </li>
+            </ul>
+          </li>
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-table"></i>
