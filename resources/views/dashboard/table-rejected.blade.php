58dedd79031b81e02bee8027a199ee7e55fd1922
michelsdev <sinambelamichel05@gmail.com>
2024-02-22 13:21:32 +0700

    modified on hold and rejected

--- resources/views/dashboard/table-rejected.blade.php
+++ resources/views/dashboard/table-rejected.blade.php
@@ -33,7 +33,7 @@
       <!-- Default box -->
       <div class="card">
         <div class="card-header">
-          <h3 class="card-title">Table Rejected</h3>
+          <h3 class="card-title">Data Rejected</h3>
 
           <div class="card-tools">
             <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
