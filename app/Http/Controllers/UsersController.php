3caedcc8d3fdcabfcdb665a85310ef5093ec312d
michelsdev <sinambelamichel05@gmail.com>
2024-03-04 14:19:19 +0700

    update users table

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,27 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Models\sdm_cuti;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class UsersController extends Controller
 {
     public function userstable()
     {
-        // Mendapatkan tanggal saat ini
-        $today = now();
 
-        // Menghitung tanggal 16 dari bulan ini
-        $start_date = $today->copy()->startOfMonth()->addDays(15);
+        $currentDate = Carbon::now();
 
-        // Menghitung tanggal 15 dari bulan depan
-        $end_date = $today->copy()->endOfMonth()->addDay()->addMonth()->subDays(15);
+        $date = Carbon::now()->subMonth();
 
-        // Jika tanggal hari ini lebih besar dari tanggal 15, gunakan bulan berikutnya sebagai akhir
-        if ($today->day > 15) {
-            $end_date = $end_date->addMonth();
+        if ($currentDate->day > 15) {
+            $date=Carbon::now();
         }
 
-        // Mengambil data berdasarkan rentang tanggal
-        $data = sdm_cuti::whereDate('tanggal_mulai', '>=', $start_date)->whereDate('tanggal_mulai', '<=', $end_date)->get();
+        $startDate = $date->copy()->startOfMonth()->addDays(14);
+        $endDate = $date->copy()->endOfMonth()->addDays(16);
+
+        $data = sdm_cuti::where('tanggal_mulai', '>=', $startDate)
+        ->where('tanggal_mulai', '<=', $endDate)
+        ->get();
+        // dd($data);
 
         return view('users.users-table', compact('data'));
     }
